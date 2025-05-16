5362c86d0232e6cd17b5f5efc17d8e8e6bea58db
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2025-05-16 17:11:21 +0530

    Update page_1.py

--- page_1.py
+++ page_1.py
@@ -54,7 +54,7 @@ def set_background(image_path):
                 text-overflow: ellipsis; /* Add ellipsis if text exceeds */
             }}
             .subheading {{
-                color: #17224d;
+                color: #00cadc;
                 font-size: 1.5em;
                 margin-top: 0.5em;
             }}
